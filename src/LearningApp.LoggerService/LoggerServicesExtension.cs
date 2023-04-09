43b3c0541b1187efb88ca114d7ed167c773ca006
VitonBaton <vitonbaton@yandex.ru>
2023-04-09 17:25:30 +0300

    Moved logger configuration to extension method

--- src/LearningApp.LoggerService/LoggerServicesExtension.cs
+++ src/LearningApp.LoggerService/LoggerServicesExtension.cs
@@ -1,10 +1,20 @@
 ﻿using LearningApp.Contracts.Services;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Serilog;
 
 namespace LearningApp.LoggerService;
 
 public static class LoggerServicesExtension
 {
+    public static WebApplicationBuilder AddSerilogLoggerProvider(this WebApplicationBuilder builder)
+    {
+        builder.Host.UseSerilog((context, config) =>
+        {
+            config.ReadFrom.Configuration(context.Configuration);
+        });
+        return builder;
+    }
     public static IServiceCollection AddLogger(this IServiceCollection services)
     {
         services.AddScoped<ILoggerManager, LoggerManager>();
