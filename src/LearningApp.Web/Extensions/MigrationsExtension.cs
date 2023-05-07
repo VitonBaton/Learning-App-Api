057f22f5b04ddc1201e762a7270e7142004018a7
VitonBaton <vitonbaton@yandex.ru>
2023-05-07 17:13:43 +0300

    added docker-compose and dockerfile

---
+++ src/LearningApp.Web/Extensions/MigrationsExtension.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LearningApp.Web.Extensions;
+
+public static class MigrationsExtension
+{
+    public static async Task MigrateDatabaseAsync<TContext>(this IServiceProvider serviceProvider,
+        CancellationToken cancellationToken)
+        where TContext : DbContext
+    {
+        await using var scope = serviceProvider.CreateAsyncScope();
+        var context = scope.ServiceProvider.GetRequiredService<TContext>();
+        var logger = scope.ServiceProvider.GetRequiredService<ILogger<TContext>>();
+
+        try
+        {
+            logger.LogInformation("Database migration started");
+            await context.Database.MigrateAsync(cancellationToken);
+
+            logger.LogInformation("Database has been migrated");
+        }
+        catch (Exception ex)
+        {
+            logger.LogCritical(ex, "An error occurred while migrating the database");
+        }
+    }
+}
