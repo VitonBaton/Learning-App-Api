2919c88039393c90b34a97a1ed508a87f1ec0846
VitonBaton <vitonbaton@yandex.ru>
2023-06-08 13:22:18 +0300

    fixed bug when when no image was specified for the test question

--- src/LearningApp.Models/DataTransferObjects/TestQuestionDto.cs
+++ src/LearningApp.Models/DataTransferObjects/TestQuestionDto.cs
@@ -5,6 +5,6 @@ public sealed class TestQuestionDto
     public int Id { get; set; }
     public string Question { get; set; }
     public int Order { get; set; }
-    public DateTime CreatedAt { get; set; }
+    public string? Image { get; set; }
     public IEnumerable<string> Answers { get; set; }
 }
